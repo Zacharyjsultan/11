db3485e80307e28142abe6d86f50d2fd37173366
Zach <Zach@Zanzibars-Computer.local>
2024-07-13 15:55:58 -0700

    pandering2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from "./logo1.png";
+// import logo from "./logo1.png";
 import "./App.css";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./Components/Landing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Is this my portfolio or just a cookbook?</p>
-        <p>Coming Sometime!</p>
-        <a
-          className="App-link"
-          href="mailto:zacharyjsultan@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Email Me!
-        </a>
-      </header>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
